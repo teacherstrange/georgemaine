39597ac446dacb3dd82a1f6964dbf21da12760df
Georgemaine Lourens <georgemaine.lourens@mollie.com>
2020-02-10 21:54:53 +0100

    Added GoogleAnalytics

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,21 @@ export default () => {
         />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-W6ZMGPB');`
+          }}
+        />
       </Head>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6ZMGPB" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`
+        }}
+      />
       <Logo />
       <CopySection>
         <Headline>Hello, my name is Georgemaine&nbsp;Lourens</Headline>
