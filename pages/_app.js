a8aa4c38ddfe0bed42ae12869162ea42bf4bb3d2
Georgemaine Lourens <georgemaine.lourens@mollie.com>
2020-02-10 22:53:57 +0100

    Added <Gtag />

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import Global from "../styles/global.css";
 import { ThemeProvider } from "styled-components";
 import React from "react";
+import Head from "next/head";
+import Router from "next/router";
+import { GA_TRACKING_ID } from "../lib/gtag";
+
+import * as gtag from "../lib/gtag";
+
+Router.events.on("routeChangeComplete", url => gtag.pageview(url));
 
 const theme = {
   colors: {
@@ -17,6 +24,55 @@ const theme = {
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>Georgemaine Lourens</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <meta name="description" content="Product designer at Mollie." />
+        <meta property="og:type" content="website" />
+        <meta
+          name="og:title"
+          property="og:title"
+          content="Georgemaine Lourens"
+        />
+        <meta
+          name="og:description"
+          property="og:description"
+          content="Product designer at Mollie."
+        />
+        <meta property="og:site_name" content="Georgemaine Lourens" />
+        <meta property="og:url" content="https://www.georgemaine.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Georgemaine Lourens" />
+        <meta
+          name="twitter:description"
+          content="Product designer at Mollie."
+        />
+        <meta name="twitter:creator" content="@georgemaine" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 import {
   Wrapper,
   CopySection,
@@ -19,32 +17,6 @@ import {
 export default () => {
   return (
     <Wrapper>
-      <Head>
-        <title>Georgemaine Lourens</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="I’m currently designing financial products at Mollie in Amsterdam. Previously I was a designer at Framer, where I designed tools and resources for designers. My current areas
-          of interest include well-crafted products, code, plants, and motion design."
-        />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-W6ZMGPB');`
-          }}
-        />
-      </Head>
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6ZMGPB" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`
-        }}
-      />
       <Logo />
       <CopySection>
         <Headline>Hello, my name is Georgemaine&nbsp;Lourens</Headline>
