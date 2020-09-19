1092ab127a1af76a7e374c96379b57a838521c93
Georgemaine <georgemaine.lourens@gmail.com>
2020-09-19 13:06:40 +0200

    Add Breakpoints to <StickyNav />

--- components/Navigation/style.tsx
+++ components/Navigation/style.tsx
@@ -4,6 +4,18 @@ import { motion } from "framer-motion";
 export const StickyNav = styled.nav`
   max-width: 290px;
   margin: 60px auto 0;
+  margin-left: auto;
+  margin-right: auto;
+  z-index: 10;
+
+  @media only screen and (max-width: 599px) {
+    position: absolute;
+
+    margin-top: 0;
+    left: 0;
+    right: 0;
+    bottom: 36px;
+  }
 `;
 
 export const StickyNavList = styled(motion.ul)`
