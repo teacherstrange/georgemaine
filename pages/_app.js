1a2b25af25d70fde9fa53bc8893c57be5a97d78f
Georgemaine <georgemaine.lourens@gmail.com>
2020-09-26 15:46:21 +0200

    Remove GTAG

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,6 @@ import Global from "../components/global.css";
 import React from "react";
 import Head from "next/head";
 import Router from "next/router";
-import { GA_TRACKING_ID } from "../lib/gtag";
-
-import * as gtag from "../lib/gtag";
-
-Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
@@ -19,7 +14,7 @@ export default function MyApp({ Component, pageProps }) {
           name="viewport"
           content="initial-scale=1, width=device-width, shrink-to-fit=no"
         />
-        <meta name="description" content="Product designer at Mollie." />
+        <meta name="description" content="Product designer at Pitch." />
         <meta property="og:type" content="website" />
         <meta
           name="og:title"
@@ -30,37 +25,16 @@ export default function MyApp({ Component, pageProps }) {
         <meta
           name="og:description"
           property="og:description"
-          content="Product designer at Mollie."
+          content="Product designer at Pitch."
         />
         <meta property="og:site_name" content="Georgemaine Lourens" />
         <meta property="og:url" content="https://www.georgemaine.com" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Georgemaine Lourens" />
-        <meta
-          name="twitter:description"
-          content="Product designer at Mollie."
-        />
+        <meta name="twitter:description" content="Product designer at Pitch." />
         <meta name="twitter:creator" content="@georgemaine" />
         <link rel="icon" href="/favico.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/favico.svg" />
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
       </Head>
       <Component {...pageProps} />
     </>
