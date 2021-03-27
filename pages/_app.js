155f811cb12d9df249cc17e7dead35bb1bc30b94
Georgemaine <georgemaine.lourens@gmail.com>
2021-03-27 19:40:19 +0100

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,11 @@
-import Global from "../components/global.css";
-import React from "react";
-import Head from "next/head";
-import Router from "next/router";
+import styles from '../app-styles.module.css'
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>Georgemaine Lourens</title>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="initial-scale=1, width=device-width, shrink-to-fit=no"
-        />
-        <meta name="description" content="Product designer at Pitch." />
-        <meta property="og:type" content="website" />
-        <meta
-          name="og:title"
-          property="og:title"
-          content="Georgemaine Lourens"
-        />
-        <meta key="og:image" name="og:image" content="/images/og-image.jpg" />
-        <meta
-          name="og:description"
-          property="og:description"
-          content="Product designer at Pitch."
-        />
-        <meta property="og:site_name" content="Georgemaine Lourens" />
-        <meta property="og:url" content="https://www.georgemaine.com" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Georgemaine Lourens" />
-        <meta name="twitter:description" content="Product designer at Pitch." />
-        <meta name="twitter:creator" content="@georgemaine" />
-        <link rel="icon" href="/favico.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/favico.svg" />
-      </Head>
+    <div className={styles.content}>
       <Component {...pageProps} />
-    </>
-  );
+    </div>
+  )
 }
+
+export default MyApp
