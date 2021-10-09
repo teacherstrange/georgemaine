42e1ac6890b93a4e9205a68011c0d8dad4d86bbe
Georgemaine <georgemaine.lourens@gmail.com>
2021-10-09 23:41:23 +0200

    Added Fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
 import "../components/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 
 function Website({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("QXIMCIGI", {
+      includedDomains: ["georgemaine.com"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
